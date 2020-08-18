91da3be2c8a5af7d970f5a886d89b6786372df90
badromance007 <nguyenpeter.edu@gmail.com>
2020-08-18 10:08:39 +0700

    update read_execel.py

--- read_excel.py
+++ read_excel.py
@@ -18,14 +18,17 @@ data =[]
 
 for i in range(sheet.nrows):
     if i >= 1:
-      print(sheet.cell_value(i, 2) + " - " +  sheet.cell_value(i, 3) ) 
-      item = {'danhmuc': sheet.cell_value(i, 1),
-              'masp': sheet.cell_value(i, 2),
-              'name': capitilize_word(sheet.cell_value(i, 3)),
-              'mota': sheet.cell_value(i, 4),
-              'giaban': str(int(sheet.cell_value(i, 5))),
-              'soluong': str(int(sheet.cell_value(i, 6))),
-              'noidung': sheet.cell_value(i, 8)}
+      print(sheet.cell_value(i, 2) + " - " +  sheet.cell_value(i, 5) ) 
+      item = {'danhmuc1': sheet.cell_value(i, 1),
+              'danhmuc2': sheet.cell_value(i, 2),
+              'danhmuc3': sheet.cell_value(i, 3),
+              'masp': sheet.cell_value(i, 4),
+              'name': capitilize_word(sheet.cell_value(i, 5)),
+              'mota': sheet.cell_value(i, 6),
+              'giaban': str(int(sheet.cell_value(i, 7))),
+              'soluong': str(int(sheet.cell_value(i, 8))),
+              'tonganh': sheet.cell_value(i, 9),
+              'noidung': sheet.cell_value(i, 10)}
       data.append(item)
 with open('data/data.json', 'w', encoding='utf-8') as f:
   json.dump(data, f, ensure_ascii=False, indent=4)
