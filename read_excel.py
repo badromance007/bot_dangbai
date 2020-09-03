88ddb876c16f54396702c9363cc759045a2f764c
badromance007 <nguyenpeter.edu@gmail.com>
2020-09-03 09:18:02 +0700

    empty data folder

--- read_excel.py
+++ read_excel.py
@@ -1,6 +1,21 @@
 import xlrd 
 import json
 from glob import glob
+import os, shutil
+
+
+
+import os, shutil
+folder = 'data'
+for filename in os.listdir(folder):
+    file_path = os.path.join(folder, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 
 def capitilize_word(txt):
